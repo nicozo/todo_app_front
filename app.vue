2070e8a2239a21d437e4e1146914a39ef043217e
nicozo <niczo.soccer161@gmail.com>
2023-01-16 13:14:29 +0900

    Update: Hello Worldを取得

--- app.vue
+++ app.vue
@@ -7,6 +7,6 @@
 </template>
 
 <script setup>
-const data = await useFetch('http://localhost:8000/api');
+const data = await useFetch('http://localhost:8000/api/message');
 console.log(data);
 </script>
